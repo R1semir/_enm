0bb2cfe70117111f8a99eb8d0b9f9ea8396f2618
Emir <emirkan.ulker9@gmail.com>
2023-12-19 14:24:02 +0300

    enm

---
+++ _enum_/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _enum_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum sehirler
+        {
+           Bulunamadı,Adana, Adıyaman, Afyon, Ağrı, Amasya, Ankara, Antalya, Artvin, Aydın
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int plaka = Convert.ToInt16(textBox1.Text);
+            sehirler s;
+            s = (sehirler)plaka;
+            label2.Text = s.ToString();
+
+        }
+    }
+}
